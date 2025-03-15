c5621454e837f1f883a8133a382336d8b6d5e7af
Gustavo Ferreira da Silva <gustavofersilva@gamil.com>
2025-03-15 00:30:44 -0300

    fix: Eslint flat format

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,12 +3,17 @@ import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 
+const parser = createConfigItem('@typescript-eslint/parser', { 
+  global: true 
+});
+
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {
     files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
     languageOptions: {
-      globals: { ...globals.browser, ...globals.node }
+      globals: { ...globals.browser, ...globals.node },
+      parser
     },
     parser: "@typescript-eslint/parser", // Define o parser do TypeScript
     plugins: [tseslint, pluginReact],      // Adiciona o plugin do TypeScript
