0b4824ecf48419c919977b0f72c00fecd9af7252
Gustavo Ferreira da Silva <gustavofersilva@gamil.com>
2025-03-15 00:49:41 -0300

    fix: removido parser inutilizado

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,7 +9,6 @@ export default [
     files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
     languageOptions: {
       globals: { ...globals.browser, ...globals.node },
-      parser,
     },
     parser: '@typescript-eslint/parser',
     plugins: [tseslint, pluginReact, pluginJs],
