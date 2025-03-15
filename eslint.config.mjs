95622c6beec45e53fd8668a3d72366f922b456f9
Gustavo Ferreira da Silva <gustavofersilva@gamil.com>
2025-03-15 00:42:40 -0300

    fix: type modules, eslint config and prettier format

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,27 +1,24 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
-import eslintPkg from 'eslint';
-const { createConfigItem } = eslintPkg;
-
-const parser = createConfigItem('@typescript-eslint/parser', { 
-  global: true 
-});
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {
-    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
     languageOptions: {
       globals: { ...globals.browser, ...globals.node },
-      parser
+      parser,
     },
-    parser: "@typescript-eslint/parser",
-    plugins: [tseslint, pluginReact, pluginJs],      
+    parser: '@typescript-eslint/parser',
+    plugins: [tseslint, pluginReact, pluginJs],
     rules: {
-      "react/react-in-jsx-scope": "off",
-      "react/jsx-uses-react": "off",
-    }
-  }, 
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-uses-react': 'off',
+      'no-console': 'warn',
+      '@typescript-eslint/no-unused-vars': 'warn',
+      'react/jsx-uses-vars': 'warn',
+    },
+  },
 ];
