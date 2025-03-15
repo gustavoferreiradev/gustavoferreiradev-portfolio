654bfcf2e4aea1faa21986f20c4995481c105ed0
Gustavo Ferreira da Silva <gustavofersilva@gamil.com>
2025-03-15 00:46:57 -0300

    fix: Convertido next.config.js para module

--- next.config.js
+++ next.config.js
@@ -1,19 +1,17 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
-  reactStrictMode: true,
-  webpack(config) {
-    config.experiments = { topLevelAwait: true };
-    config.module.rules.push({
-      test: /\.(js|mjs|jsx|ts|tsx)$/,
-      exclude: /node_modules/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          plugins: ['@babel/plugin-syntax-top-level-await'],
-        },
+export const reactStrictMode = true;
+export function webpack(config) {
+  config.experiments = { topLevelAwait: true };
+  config.module.rules.push({
+    test: /\.(js|mjs|jsx|ts|tsx)$/,
+    exclude: /node_modules/,
+    use: {
+      loader: 'babel-loader',
+      options: {
+        plugins: ['@babel/plugin-syntax-top-level-await'],
       },
-    });
+    },
+  });
 
-    return config;
-  },
-};
+  return config;
+}
