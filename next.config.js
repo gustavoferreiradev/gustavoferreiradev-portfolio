e98b2775abb2339fed9ca262753cfa95c023c909
Gustavo Ferreira da Silva <gustavofersilva@gamil.com>
2025-03-15 01:25:14 -0300

    chore: ajustada a configuração do NextJS

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 export const reactStrictMode = true;
 export function webpack(config) {
-  config.experiments = { topLevelAwait: true };
+  config.experiments = { topLevelAwait: true, layers: true };
   config.module.rules.push({
     test: /\.(js|mjs|jsx|ts|tsx)$/,
     exclude: /node_modules/,
